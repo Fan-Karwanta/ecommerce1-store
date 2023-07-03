c09de7d21eadca857357090a351eb430350483cc
Fan Karwanta <135393498+Fan-Karwanta@users.noreply.github.com>
2023-07-03 15:46:51 +0800

    Update footer.tsx
    update on copyright symbol

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; © 2023 By Fan Karwanta. All rights reserved.
+          &copy; 2023 By Fan Karwanta. All rights reserved.
         </p>
       </div>
     </footer>
